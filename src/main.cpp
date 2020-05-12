22de0362fd3bffc62ceac4b2b301b45e6cf2fd40
Jedzia <jed69@gmx.de>
2020-05-12 17:33:03 +0200

    added arduino core library and a blink example

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
-/* 
- * File: blink.c
- * Description: Toggels pin PB0 every 500ms
- * From: C-Programmierung mit AVR-GCC
- */
+/*
+  Blink
 
-#define F_CPU 8000000UL
+  Turns an LED on for one second, then off for one second, repeatedly.
 
-#include <avr/io.h>
-#include <util/delay.h>
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
 
-int main (void) {
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
 
-   DDRB |= (1 << PB0);
+  This example code is in the public domain.
 
-   while(1) {
-       PORTB ^= (1 << PB0);
-       _delay_ms(500);
-   }
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
 
-   return 0;
+#include <Arduino.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
 }
